75cab5026be56d3be45c39e4e986ad749e48be98
Aaron Harris <aaronsharris@gmail.com>
2021-10-08 11:36:19 -0500

    created restrict.js and updated user for admin

--- .history/models/user_20211008113322.js
+++ .history/models/user_20211008113322.js
@@ -9,7 +9,7 @@ const User = new Schema(
     },
     email: { type: String, required: true },
     password_digest: { type: String, required: true, select: false },
-    admin: {type: Boolean, required: true},
+    
   },
   { timestamps: true }
 )
