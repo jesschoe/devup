0c6d9591cbc926ba92a73ed071b739c1af9068d6
Aaron Harris <aaronsharris@gmail.com>
2021-10-11 14:43:43 -0500

    basic styling to home screen and buttons

--- client/.history/src/screens/Home/Home_20211011143159.jsx
+++ client/.history/src/screens/Home/Home_20211011143159.jsx
@@ -6,8 +6,8 @@ const Home = () => {
   return (
     <Layout>
       <div className="min-h-screen min-w-max flex items-center justify-center ">
-        <div className="grid grid-cols-3 w-full gap-y-6">
-          <div className="bg-black p-10 col-span-3">
+        <div className="grid grid-cols-3 gap-y-10 w-full">
+          <div className="bg-black p-10 col-span-3 my-10">
               <div className=" bg-hero bg-cover bg-center h-96 flex items-center justify-end">
                 <div className="pr-10">
                     <h1 className="text-orange text-5xl text-left">Devup<br/>your space</h1>
@@ -18,7 +18,7 @@ const Home = () => {
                     </div>
               </div>
           </div>
-          <div className="bg-black py-10 col-span-3">
+          <div className="bg-black p-10 col-span-3 my-10">
               <div className=" bg-setup bg-cover bg-center h-96 flex items-center justify-start">
                     <div className="pl-10">
                     <h1 className="text-orange text-5xl text-left">Set Up</h1>
@@ -29,7 +29,7 @@ const Home = () => {
                     </div>
               </div>
           </div>
-          <div className="bg-black py-10 col-span-3">
+          <div className="bg-black p-10 col-span-3 my-10">
               <div className=" bg-gearup bg-cover bg-center h-96 flex items-center justify-end">
                 <div className="pr-10">
                     <h1 className="text-orange text-5xl text-left">Gear Up</h1>
@@ -40,11 +40,11 @@ const Home = () => {
                     </div>
               </div>
           </div>
-          <div className="bg-black py-10 col-span-3">
+          <div className="bg-black p-10 col-span-3 my-10">
               <div className=" bg-levelup bg-cover bg-center h-96 flex items-center justify-start">
                     <div className="pl-10">
                     <h1 className="text-orange text-5xl text-left">Level Up</h1>
-                    <h2 className="text-white text-lg">Optimize your space<br/>for efficiency and convienience</h2>
+                    <h2 className="text-white text-lg">Optimize your space<br/>for efficien and style</h2>
                     <Link to='/products'>
                     <button className="flex items-center px-6 text-xl my-4 rounded text-white bg-orange">Browse all</button>
                     </Link>
--- client/.history/tailwind.config_20211011134248.js
+++ client/.history/tailwind.config_20211011134248.js
@@ -8,8 +8,8 @@ module.exports = {
       backgroundImage: {
         'hero': 'url("https://res.cloudinary.com/devupapp/image/upload/v1633969053/devup/homeimage_jaucdr.png")',
         'setup': 'url("https://res.cloudinary.com/devupapp/image/upload/v1633967360/devup/desk4_d9n5j0.jpg")',
-        'gearup': 'url("https://res.cloudinary.com/devupapp/image/upload/v1633979273/devup/devup-headphoneGirl_gzj2iv.png")',
-        'levelup': 'url("https://res.cloudinary.com/devupapp/image/upload/v1633979303/devup/desk-asc_iys574.jpg")'
+        'gearup': 'url("https://res.cloudinary.com/devupapp/image/upload/v1633967712/devup/devup-headphoneGirl_gzj2iv.png")'
+        'gearup': 'url("https://res.cloudinary.com/devupapp/image/upload/v1633967712/devup/devup-headphoneGirl_gzj2iv.png")'
       }
     },
     colors: {
