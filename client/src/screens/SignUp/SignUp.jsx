23bee7b17a9737a9360323fe6319b776d2ad5ebd
Jessica Choe <jessj.choe@gmail.com>
2021-10-12 05:00:20 +0900

    styling for signin and signup

--- client/src/screens/SignUp/SignUp.jsx
+++ client/src/screens/SignUp/SignUp.jsx
@@ -59,87 +59,87 @@ const SignUp = (props) => {
 
   return (
       <Layout>
-          <div className="min-h-screen min-w-max">
-          <div className="flex flex-col items-center">
-          <div className="self-center mb-2 text-xl font-bold text-left text-white sm:text-2xl dark:text-white">
+          <div className="flex flex-col items-center min-h-screen min-w-max">
+          <div className="flex flex-col items-center max-w-md mb-10">
+          <div className="self-start mt-8 mb-2 text-xl font-extrabold text-white sm:text-2xl dark:text-white">
             Sign Up
           </div>
-        <div className="flex flex-col justify-center items-center max-w-md px-4 py-8 bg-black sm:px-6 md:px-8 lg:px-10">
-          <div className="p-6 mt-8">
+        <div className="flex flex-col justify-center items-center max-w-lg bg-black sm:px-6 md:px-8 lg:px-10">
+          <div className="p-6 mt-4">
             <form onSubmit={onSignUp}>
               <div className="flex flex-col mb-2">
                 <div className=" relative ">
-                  <label>Name</label>
+                  <label className="text-xs">Name</label>
                   <input
-                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-primary placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
+                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-sm text-white placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
                     required
                     type="text"
                     name='name'
                     value={name}
                     onChange={handleChange}
-                    placeholder="name"
+                    placeholder=""
                   />
                 </div>
               </div>
               <div className="flex flex-col mb-2">
                 <div className=" relative ">
-                  <label>Username</label>
+                  <label className="text-xs">Username</label>
                   <input
-                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-primary placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
+                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-sm text-white placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
                     required
                     type="text"
                     name='username'
                     value={username}
                     onChange={handleChange}
-                    placeholder="username"
+                    placeholder=""
                   />
                 </div>
               </div>
               <div className="flex flex-col mb-2">
                 <div className=" relative ">
-                  <label>Email</label>
+                  <label className="text-xs">E-mail</label>
                   <input
-                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-primary placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
+                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-sm text-white placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
                     required
                     type="text"
                     name='email'
                     value={email}
                     onChange={handleChange}
-                    placeholder="email"
+                    placeholder=""
                   />
                 </div>
               </div>
               <div className="flex flex-col mb-2">
                 <div className=" relative ">
-                  <label>Password</label>
+                  <label className="text-xs">Password</label>
                   <input
-                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-primary placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
+                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-sm text-white placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
                     required
                     type="password"
                     name="password"
                     value={password}
                     onChange={handleChange}
-                    placeholder="password"
+                    placeholder=""
                   />
                 </div>
               </div>
               <div className="flex flex-col mb-2">
                 <div className=" relative ">
-                  <label>Confirm Password</label>
+                  <label className="text-xs">Confirm Password</label>
                   <input
-                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-primary placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
+                    className="flex-1 border border-orange w-full py-2 px-4 bg-black text-sm text-white placeholder-primary focus:outline-none focus:ring-2 focus:ring-purple focus:border-transparent"
                     required
                     type="password"
                     name="confirmPassword"
                     value={confirmPassword}
                     onChange={handleChange}
-                    placeholder="password"
+                    placeholder=""
                   />
                 </div>
               </div>
-              <div className="flex w-full my-4">
+              <div className="flex mt-8 w-full my-4 justify-center">
                 <div
-                  className="py-2 px-4 bg-orange hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+                  className="py-2 px-4 bg-orange hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-1/2 transition ease-in duration-200 text-center text-base font-semibold focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-md"
                   type="submit"
                 >
                   {renderError()}
