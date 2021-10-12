98cf0cc22998e071df6ad769bd913ac445c2d1fd
Aaron Harris <aaronsharris@gmail.com>
2021-10-11 22:47:00 -0500

    deleted history

--- .history/client/src/components/Layout/Layout_20211009152019.jsx
+++
@@ -1,2 +0,0 @@
-import Nav from "../Nav/Nav";
-import Footer from "../Footer/Footer";
--- .history/client/src/components/SignIn/SignIn_20211010132530.jsx
+++
@@ -1,9 +0,0 @@
-import React, { useLayoutEffect } from 'react'
-
-const SignIn = () => {
-    return (
-       <useLayoutEffect
-    )
-}
-
-export default SignIn
