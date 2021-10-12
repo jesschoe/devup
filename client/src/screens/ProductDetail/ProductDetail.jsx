64ac10f0d9e04c0c113b2bae0c23f7b90a2f1221
Tyler Washington <tylerwashington@Tylers-MacBook-Air.local>
2021-10-12 15:36:45 -0400

    styling the product detail page

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -28,64 +28,73 @@ const ProductDetail = () => {
 
   return (
     <Layout>
-      <div className="flex justify-center items-center min-h-screen">
-    <div class="flex max-w-2xl mx-auto overflow-hidden bg-black rounded justify-center items-center my-24">
-
-        <div class="w-2/3 p-4 md:p-4">
-            <h1 class="text-3xl font-bold text-white">{product.name}</h1>
-
-            <p class="mt-2 text-xs text-white">{product.description}</p>
-            <p class="mt-2 text-xs text-white">
-            <ul className="">{product.details.map((detail) => {
-                return <li> - {detail}</li>
-              })}</ul>
-            </p>
-            <div class="flex justify-between mt-3 item-center">
-                <h1 class="text-lg font-bold text-white md:text-xl">{`$${product.price}`}</h1>
+      <div class="flex justify-center flex-column items-center min-h-screen">
+        <div class="flex max-w-5xl mx-auto overflow-hidden bg-black rounded justify-center items-center my-24">
+          <div class="flex flex-row ">
+            <div class="flex justify-center content-center">
+              <img class="max-h-64 ax-w-xs overflow-hidden m-10" src={product.imgURL} alt="" />
             </div>
-            <Link to="">
-                <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded m-4">See More</button>
+            <div class=" m-10">
+              <h1 class="text-3xl font-bold text-white mb-5">{product.name}</h1>
+              <p class="max-w-sm text-xs mb-8 text-white">
+                <ul className="">{product.details.map((detail) => {
+                  return <li> - {detail}</li>
+                })}</ul>
+              </p>
+              <h1 class="text-lg font-bold text-white md:text-xl">{`$${product.price}`}</h1>
+              <div>
+                <Link to="">
+                  <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded m-4">See More</button>
                 </Link>
                 <Link to="">
-                <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded ">See More</button>
+                  <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded ">See More</button>
                 </Link>
+              </div>
+            </div>
+          </div>
         </div>
-        <div class="w-full my-auto">
-          <img className="overflow-hidden"src={product.imgURL} alt="" />
-        </div>
-    </div>
-    </div>
+      </div>
     </Layout>
   );
 };
 
-export default ProductDetail;
 
-{
-  /* <div className="flex justify-center min-h-screen min-w-max">
-        <div class="bg-black flex justify-center max-w-56">
-          <div class="">
-            <div class="">
-              <h1 class="">{product.name}</h1>
-              <h1 className="">{product.description}</h1>
+
+
+
+{/* <div className="flex justify-center items-center min-h-screen">
+        <div class="flex max-w-5xl mx-auto overflow-hidden bg-black rounded justify-center items-center my-24">
+
+          <div class="w-2/3 p-96 md:p-4">
+            <h1 class="text-3xl font-bold text-white">{product.name}</h1>
+
+            <p class="mt-2 text-xs text-white">{product.description}</p>
+            <div class="flex justify-between mt-3 item-center">
+              <h1 class="text-lg font-bold text-white md:text-xl">{`$${product.price}`}</h1>
+            </div>
+            <Link to="">
+              <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded m-4">See More</button>
+            </Link>
+            <Link to="">
+              <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded ">See More</button>
+            </Link>
+          </div>
+
+
+
+
+          <div class="flex-column m-10  ">
+            <img class="max-h-80 overflow-hidden relative right-10 m-10" src={product.imgURL} alt="" />
+            <p class="max-w-sm max-w-30 text-xs mb-8 text-white">
               <ul className="">{product.details.map((detail) => {
                 return <li> - {detail}</li>
               })}</ul>
-              <div className="">
-                <button className=""><Link to={"/products"}>See More</Link></button>
-                <button className=""><Link to={`/products/${id}/edit`}>Edit Product</Link></button>
-              </div>
-            </div>
-            <div class="">
-              <img class="" src={product.imgURL} alt={product.title} />
-              <h1 className="">{product.price}</h1>
-            </div>
+            </p>
           </div>
         </div>
+      </div> */}
 
-        <div className="product-detail-column-two">
-          <img className="product-title" src={product.imgURL} alt={product.title} />
-          <h1 className="product-price">{product.price}</h1>
-        </div>
-      </div> */
-}
+
+export default ProductDetail;
+
+
