3f00e33ac38f36c2c03d9b9633a61fcf7a0cc698
Aaron Harris <aaronsharris@gmail.com>
2021-10-12 18:47:32 -0500

    styling products screen

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -4,7 +4,7 @@ const Product = (props) => {
   return (
     <>
       <Link to={`/products/${props._id}`}>
-        <div className="grid grid-row-3 m-4 w-64 h-72 bg-black border-2 border-black border-transparent hover:border-purple">
+        <div className="grid grid-row-3 mx-4 w-64 h-72 bg-black border-2 border-black border-transparent hover:border-purple">
           <img className="h-48 w-full" src={props.imgURL} alt="" />
           <div className="text-orange font-bold p-2">{props.name}</div>
           <div className="p-2">{`$${props.price}`}</div>
