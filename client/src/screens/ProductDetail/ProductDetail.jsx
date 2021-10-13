6491aa761763927c88bc4a988ca3a79cee90603a
Tyler Washington <tylerwashington@Tylers-MacBook-Air.local>
2021-10-13 08:54:02 -0400

    styled product detail page. Description width is inconsistent

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -27,28 +27,33 @@ const ProductDetail = () => {
   return (
     <Layout>
       <div class="flex justify-center flex-column items-center min-h-screen">
-        <div class="flex max-w-5xl mx-auto overflow-hidden bg-black rounded justify-center items-center my-24">
+        <div class="flex flex-column flex-wrap max-w-5xl mx-auto overflow-hidden bg-black rounded justify-center items-center my-24 mb-96 ">
           <div class="flex flex-row ">
             <div class="flex justify-center content-center">
-              <img class="max-h-64 ax-w-xs overflow-hidden m-10" src={product.imgURL} alt="" />
+              <img class="max-h-64 max-w-lg overflow-hidden m-10" src={product.imgURL} alt="" />
             </div>
             <div class=" m-10">
-              <h1 class="text-3xl font-bold text-white mb-5">{product.name}</h1>
+              <h1 class="text-3xl font-bold stroke-orange text-white mb-5  ">{product.name}</h1>
+              <h3 class="text-lg font-bold text-white mb-1.5">Features</h3>
               <p class="max-w-sm text-xs mb-8 text-white">
                 <ul className="">{product.details.map((detail) => {
-                  return <li> - {detail}</li>
+                  return <li>- {detail}</li>
                 })}</ul>
               </p>
-              <h1 class="text-lg font-bold text-white md:text-xl">{`$${product.price}`}</h1>
-              <div>
+              <h1 class="text-lg font-bold text-white md:text-xl -mt-3.5 mb-1.5">{`$${product.price}`}</h1>
+              <div class="relative right-5">
                 <Link to="">
-                  <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded m-4">See More</button>
+                  <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded m-4 h-8 w-40">See More</button>
                 </Link>
                 <Link to="">
-                  <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded ">See More</button>
+                  <button class="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded m-4 h-8 w-40">Edit Product</button>
                 </Link>
               </div>
             </div>
+          </div>
+          <div class="flex flex-column flex-wrap max-w-3xl m-3 mb-16 relative right-5">
+            <div class="mb-1.5 text-lg font-bold text-white">Description</div>
+            <p class="text-white" >{product.description}</p>
           </div>
         </div>
       </div>
