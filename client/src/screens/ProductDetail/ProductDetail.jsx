768632e234f4e88f777126a99ff57a6357458e27
Jessica Choe <jessj.choe@gmail.com>
2021-10-14 04:19:28 +0900

    update seed file with png images

--- client/src/screens/ProductDetail/ProductDetail.jsx
+++ client/src/screens/ProductDetail/ProductDetail.jsx
@@ -30,32 +30,32 @@ const ProductDetail = () => {
       <div className="container">
       <div className="w-9/12 flex flex-col mt-10">
         <div className="flex flex-col overflow-y-scroll bg-black justify-center items-center mb-20">
-          <div className="flex flex-col flex-wrap lg:flex-row ">
+          <div className="flex flex-col flex-wrap md:flex-row">
             <div className="flex justify-center content-center">
               <img className="max-h-64 max-w-lg m-10" src={product.imgURL} alt="" />
             </div>
-            <div className="flex flex-col flex-wrap md:m-10 m-4">
-              <div className="flex text-3xl font-black text-white mb-5">{product.name}</div>
+            <div className="flex flex-col flex-wrap md:m-10 m-4 p-8">
+              <div className="text-3xl font-black text-white mb-5">{product.name}</div>
               <div className="text-lg font-bold text-white mb-1.5">Features</div>
               <div className="text-xs mb-8 text-white">
                 <ul className="">{(product.details).split("/n").map((detail, i) => {
-                  return <li className="p-1" key={i}>- {detail}</li>
+                  return <li className="p-1 list-disc" key={i}>{detail}</li>
                 })}</ul>
               </div>
               <h1 className="text-lg font-bold text-white md:text-xl -mt-3.5 mb-1.5">{`$${product.price}`}</h1>
               <div className="flex flex-col sm:flex-row">
-                <Link to="">
+                <a href={product.productURL} target="_blank">
                   <button className="mr-8 px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded my-4 h-8 w-40">See More</button>
-                </Link>
-                <Link to="">
+                </a>
+                <Link to={`/${id}/edit`}>
                   <button className="px-2 py-1 text-xs font-bold text-white bg-orange uppercase rounded my-4 h-8 w-40">Edit Product</button>
                 </Link>
               </div>
             </div>
           </div>
           <div className="flex flex-col flex-wrap mb-16 p-8">
-            <div className="mb-1.5 text-lg font-bold text-white">Description</div>
-            <p className="text-white" >{product.description}</p>
+            <div className="text-lg font-bold text-white pl-10">Description</div>
+            <p className="text-white px-10" >{product.description}</p>
           </div>
         </div>
         <Footer />
