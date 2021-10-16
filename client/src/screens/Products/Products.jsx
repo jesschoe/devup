545b57da7915d905b00a4783f539c5ca50d3e743
Jessica Choe <jessj.choe@gmail.com>
2021-10-17 04:33:25 +0900

    edit products

--- client/src/screens/Products/Products.jsx
+++ client/src/screens/Products/Products.jsx
@@ -83,7 +83,7 @@ const Products = (props) => {
     <Layout user={props.user} admin={props.admin}>
       <div className="container">
         <div className="w-9/12 flex flex-col">
-          <div className="mt-10 mb-4 text-3xl font-black text-white self-start ml-20">
+          <div className="mt-10 mb-4 text-3xl font-black text-orange self-start ml-20">
             <div className="mb-2">
               {sortTitle}
             </div>
