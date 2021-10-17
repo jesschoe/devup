f32ad835a092fcb183c62427e8962b8e6e40aa11
Jessica Choe <jessj.choe@gmail.com>
2021-10-17 09:20:01 +0900

    fix home screen responsiveness

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -8,16 +8,16 @@ const Home = (props) => {
     <Layout user={props.user} setUser={props.setUser} admin={props.admin} >
       <div className="container-home" >
         <div className="section">
-          <div className="bg-black py-20 md:my-24 my-20">
-            <div className="bg-hero bg-cover bg-right h-72 flex items-center justify-end md:h-96 md:bg-cover lg:bg-contain lg:bg-no-repeat md:bg-center">
+          <div className="bg-black py-16 my-12 my-16 md:py-20 md:my-24 md:my-20">
+            <div className="bg-hero bg-cover bg-right h-56 flex items-center justify-end md:h-96 md:bg-cover lg:bg-contain lg:bg-no-repeat md:bg-center">
               <div className="pr-12 md:pr-24">
-                <h1 className="text-orange text-5xl text-left font-serif">
+                <h1 className="text-orange text-4xl md:text-5xl text-left font-serif">
                   DevUp
                 </h1>
-                <h1 className="text-orange text-5xl text-left font-serif mb-4">
+                <h1 className="text-orange text-4xl md:text-5xl text-left font-serif mb-4">
                   your space
                 </h1>
-                <h2 className="text-white text-2xl">
+                <h2 className="text-white text-xl md:text-2xl">
                   A quality setup
                   <br />
                   leads to quality code
@@ -29,7 +29,7 @@ const Home = (props) => {
                     keyword: ""
                   }
                 }}>
-                  <button className="flex items-center px-6 py-2 text-xl my-6 rounded text-white bg-orange ">
+                  <button className="flex items-center px-6 py-2 text-base md:text-xl my-6 rounded text-white bg-orange ">
                     Browse All
                   </button>
                 </Link>
@@ -38,11 +38,11 @@ const Home = (props) => {
           </div>
         </div>
         <div className="section" >
-          <div className="bg-black py-20 lg:my-36 my-20">
-            <div className="bg-setup bg-cover md:bg-center h-96 flex items-center justify-start">
+          <div className="bg-black py-16 my-12 my-16 md:py-20 md:my-24 md:my-20">
+            <div className="bg-setup bg-cover h-56 md:bg-center h-96 flex items-center justify-start">
               <div className="pl-24">
-                <h1 className="text-orange text-5xl text-left font-serif mb-2">Set Up</h1>
-                <h2 className="text-white text-lg mt-4">
+                <h1 className="text-orange text-4xl md:text-5xl text-left font-serif">Set Up</h1>
+                <h2 className="text-white text-xl md:text-2xl mt-4">
                   Customize your space
                   <br />
                   for comfort and style
@@ -54,7 +54,7 @@ const Home = (props) => {
                     keyword: ""
                   }
                 }}>
-                  <button className="flex items-center px-6 py-2 text-xl my-4 rounded text-white bg-orange">
+                  <button className="flex items-center px-6 py-2 text-base md:text-xl my-6 rounded text-white bg-orange">
                     Browse Furniture
                   </button>
                 </Link>
@@ -63,11 +63,11 @@ const Home = (props) => {
           </div>
         </div>
         <div className="section">
-          <div className="bg-black py-20 lg:my-36 my-20">
-            <div className=" bg-gearup bg-cover bg-center h-96 flex items-center justify-end">
-              <div className="pr-20">
-                <h1 className="text-orange text-5xl text-left font-serif mb-2">Gear Up</h1>
-                <h2 className="text-white text-lg mt-4">
+          <div className="bg-black py-16 my-12 my-16 md:py-20 md:my-24 md:my-20">
+            <div className="bg-gearup bg-cover h-56 bg-right md:bg-center h-96 flex items-center justify-end">
+              <div className="pr-12 md:pr-24">
+                <h1 className="text-orange text-4xl md:text-5xl text-left font-serif">Gear Up</h1>
+                <h2 className="text-white text-xl md:text-2xl mt-4">
                   Maximize your productivity
                   <br />
                   with the latest tech gear
@@ -79,7 +79,7 @@ const Home = (props) => {
                     keyword: ""
                   }
                 }}>
-                  <button className="flex items-center px-6 py-2 text-xl my-4 rounded text-white bg-orange">
+                  <button className="flex items-center px-6 py-2 text-base md:text-xl my-6 rounded text-white bg-orange">
                     Browse Gear
                   </button>
                 </Link>
@@ -88,11 +88,11 @@ const Home = (props) => {
           </div>
         </div>
         <div className="section" >
-          <div className="bg-black py-20 lg:my-36 my-20">
+          <div className="bg-black py-16 my-12 my-16 md:py-20 md:my-24 md:my-20">
             <div className=" bg-levelup bg-cover bg-center h-96 flex items-center justify-start">
               <div className="pl-20">
-                <h1 className="text-orange text-5xl text-left font-serif mb-2">Level Up</h1>
-                <h2 className="text-white text-lg mt-4">
+                <h1 className="text-orange text-4xl md:text-5xl text-left font-serif">Level Up</h1>
+                <h2 className="text-white text-xl md:text-2xl mt-4">
                   Optimize your space
                   <br />
                   for efficiency and convienience
@@ -104,7 +104,7 @@ const Home = (props) => {
                     keyword: ""
                   }
                 }}>
-                  <button className="flex items-center px-6 py-2 text-xl my-4 rounded text-white bg-orange">
+                  <button className="flex items-center px-6 py-2 text-base md:text-xl my-6 rounded text-white bg-orange">
                     Browse Accessories
                   </button>
                 </Link>
